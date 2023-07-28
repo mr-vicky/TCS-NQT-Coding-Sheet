59a3ab30d8582d9466eb5505f93d47bead008921
Vivek Sarade <viveksarade4949@gmail.com>
2023-07-28 23:55:05 +0530

    String

---
+++ GFG TCS Sheet/02]. Problems on String/03_ASCII_value_of_a_Character.cpp
@@ -0,0 +1,25 @@
+/*
+Given a character, we need to print its ASCII value in C/C++/Java/Python. 
+
+Examples :
+
+Input : a 
+Output : 97
+
+Input : D
+Output : 68
+
+
+*/ 
+
+#include<iostream>
+using namespace std;
+
+int get_ASCII_value(char c){
+    return c;
+}
+
+int main(){
+    cout<<get_ASCII_value('a')<<endl;
+    return 0;
+}
